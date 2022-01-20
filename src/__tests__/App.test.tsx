e961be25975d8304c2ae98884a6aee8fc9803e45
Juan Carlos Cruz <juancarloscruz278@gmail.com>
2022-01-20 00:19:17 -0500

    feat: adds test for Header

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,8 +1,14 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
+import React from 'react';
 import App from '../App';
 
 test('renders learn react link', () => {
-  const comp = render(<App />);
-  expect(comp).toBeTruthy();
+    const comp = render(<App />);
+    expect(comp).toBeTruthy();
+});
+
+test('Check Header is rendered', () => {
+    render(<App />);
+    const header = screen.getByTestId('header');
+    expect(header).toBeTruthy();
 });
