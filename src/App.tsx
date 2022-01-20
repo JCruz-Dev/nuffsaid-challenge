6641c1272e226cfe15df0eab2afbf6bcd936001f
Juan Carlos Cruz <juancarloscruz278@gmail.com>
2022-01-20 16:08:41 -0500

    refactor: refactors App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,13 @@
+import { Main } from 'components/common/Main';
+import { Header } from 'components/ui/Header';
 import { FC } from 'react';
-import { Header } from './components/ui/Header';
 
 const App: FC<{}> = () => {
-    // const [messages, setMessages] = useState<Message[]>([]);
-
-    // useEffect(() => {
-    //     const cleanUp = generateMessage((message: Message) => {
-    //         setMessages((oldMessages) => [...oldMessages, message]);
-    //     });
-    //     return cleanUp;
-    // }, [setMessages]);
-
     return (
-        <div>
+        <>
             <Header text='nuffsaid.com Coding Challenge' />
-            {/* {messages?.map?.(msg => <div key={msg?.message}>{msg?.message}</div>)} */}
-        </div>
+            <Main />
+        </>
     );
 };
 
