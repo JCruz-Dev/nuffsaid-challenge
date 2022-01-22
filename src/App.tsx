dd6037f88153b85c6582f8be92c3a4a08b62dfb4
Juan Carlos Cruz <juancarloscruz278@gmail.com>
2022-01-21 20:31:27 -0500

    refactor: removes header from App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import { Main } from 'components/common/Main';
-import { Header } from 'components/ui/Header';
 import { FC } from 'react';
 
 const App: FC<{}> = () => {
     return (
         <>
-            <Header text='nuffsaid.com Coding Challenge' />
             <Main />
         </>
     );
