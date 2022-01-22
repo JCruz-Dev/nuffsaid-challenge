baa9d1fd82b25dc429b5c8dd061e3fd939a99238
Juan Carlos Cruz <juancarloscruz278@gmail.com>
2022-01-21 20:33:32 -0500

    refactor: refactors main component

--- src/components/common/Main/Main.tsx
+++ src/components/common/Main/Main.tsx
@@ -1,34 +1,13 @@
-import { Button } from '@material-ui/core';
-import generateMessage, { Message } from 'Api';
+import ButtonContainer from 'components/shared/ButtonContainer';
 import Container from 'components/shared/Container';
-import { FC, useEffect, useState } from 'react';
+import { FC } from 'react';
+import { ColumnsWrapper } from '../ColumsWrapper';
 
-const Main: FC<{}> = () => {
-    const [messages, setMessages] = useState<Message[]>([]);
-    const [showMessages, setShowMessages] = useState<boolean>(false);
-
-    useEffect(() => {
-        const cleanUp = generateMessage((message: Message) => {
-            showMessages &&
-                setMessages((oldMessages) => [...oldMessages, message]);
-        });
-        return cleanUp;
-    }, [setMessages, showMessages]);
-
-    const handleShowMessage = () => {
-        setShowMessages(!showMessages);
-    };
+const Main: FC = () => {
     return (
         <Container>
-            <Button variant='contained' onClick={handleShowMessage}>
-                {showMessages ? 'Stop' : 'Start'}
-            </Button>
-            <Button variant='contained'>Clear</Button>
-            <div>
-                {messages?.map?.((msg) => (
-                    <div key={msg?.message}>{msg?.message}</div>
-                ))}
-            </div>
+            <ButtonContainer />
+            <ColumnsWrapper />
         </Container>
     );
 };
