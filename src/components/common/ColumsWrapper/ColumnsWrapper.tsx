c23def8fb6173d8c236faa4d2a6ad5c06635cd41
Juan Carlos Cruz <juancarloscruz278@gmail.com>
2022-01-21 22:09:52 -0500

    refactor: refactors Column Wrapper

--- src/components/common/ColumsWrapper/ColumnsWrapper.tsx
+++ src/components/common/ColumsWrapper/ColumnsWrapper.tsx
@@ -1,6 +1,7 @@
 import generateMessage, { Message } from 'Api';
+import { Notification } from 'components/shared/Notification';
 import { AppContext } from 'context/AppContext';
-import { FC, useContext, useEffect } from 'react';
+import { FC, useContext, useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { Column } from '../Column';
 
@@ -11,6 +12,13 @@ const GridStyled = styled.div`
 `;
 const ColumnsWrapper: FC = () => {
     const { messages, setMessages, showMessages } = useContext(AppContext);
+    const [open, setOpen] = useState(false);
+    const [errors, setErrors] = useState<Message[]>([]);
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
     useEffect(() => {
         const cleanUp = generateMessage((message: Message) => {
             showMessages &&
@@ -19,8 +27,22 @@ const ColumnsWrapper: FC = () => {
         return cleanUp;
     }, [setMessages, showMessages]);
 
+    useEffect(() => {
+        if (messages[messages.length - 1]?.priority === 0) {
+            setErrors(messages.filter((message) => message.priority === 0));
+            setOpen(true);
+        }
+    }, [messages]);
+
     return (
         <>
+            {open && (
+                <Notification
+                    message={errors[errors.length - 1].message}
+                    open={open}
+                    customClose={handleClose}
+                />
+            )}
             <GridStyled>
                 <Column
                     titleMessage='Error Type 1'
